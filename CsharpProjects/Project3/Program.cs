bc54c9ff896682039e23d770335e2d6aa716cea2
Jdilsham <janithadilsham@gmail.com>
2025-05-24 23:31:04 +0530

    numberTypes

--- CsharpProjects/Project3/Program.cs
+++ CsharpProjects/Project3/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Signed integral types:");
+﻿/*Console.WriteLine("Signed integral types:");
 
 Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
 Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
@@ -18,3 +18,104 @@ Console.WriteLine("Floating point types:");
 Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
 Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
 Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+*/
+
+//Correct
+/*
+int first = 2;
+string second = "4";
+string result = first + second;
+Console.WriteLine(result);
+
+//Wrong
+int first = 2;
+string second = "4";
+int result = first + second;
+Console.WriteLine(result);
+*/
+
+/*
+int myInt = 3;
+Console.WriteLine($"int: {myInt}");
+
+decimal myDecimal = myInt;
+Console.WriteLine($"decimal: {myDecimal}");
+*/
+
+/*
+decimal myDecimal = 3.14m;
+Console.WriteLine($"decimal: {myDecimal}");
+
+int myInt = (int)myDecimal;
+Console.WriteLine($"int: {myInt}");
+*/
+
+/*
+decimal myDecimal = 1.23456789m;
+float myFloat = (float)myDecimal;
+
+Console.WriteLine($"Decimal: {myDecimal}");
+Console.WriteLine($"Float  : {myFloat}");
+
+int first = 5;
+int second = 7;
+string message = first.ToString() + second.ToString();
+Console.WriteLine(message);
+
+string value1 = "5";
+string value2 = "7";
+int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+Console.WriteLine(result);
+*/
+
+/*
+string value = "102";
+int result = 0;
+if (int.TryParse(value, out result))
+{
+Console.WriteLine($"Measurement: {result}");
+}
+else
+{
+    Console.WriteLine("Unable to report the measurement.");
+}
+Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+
+
+string value = "bad";
+int result = 0;
+if (int.TryParse(value, out result))
+{
+Console.WriteLine($"Measurement: {result}");
+}
+else
+{
+Console.WriteLine("Unable to report the measurement.");
+}
+
+if (result > 0)
+Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+*/
+
+/*
+string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+
+decimal total = 0m;
+string message = "";
+
+foreach (var value in values)
+{
+    decimal number; // stores the TryParse "out" value
+    if (decimal.TryParse(value, out number))
+    {
+        total += number;
+    } else
+    {
+        message += value;
+    }
+}
+
+Console.WriteLine($"Message: {message}");
+Console.WriteLine($"Total: {total}");
+*/
+
