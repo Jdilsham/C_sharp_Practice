3e0ff59c3f74c029aeb0818e8f6926d2b3310c50
Jdilsham <janithadilsham@gmail.com>
2025-05-25 22:07:32 +0530

    sizeof()

--- CsharpProjects/Project3/Program.cs
+++ CsharpProjects/Project3/Program.cs
@@ -341,3 +341,39 @@ Console.WriteLine(comparisonMessage);
 */
 
 
+//Console.WriteLine("string's IndexOf() and LastIndexOf()");
+
+/*
+string message = "Find what is (inside the parentheses)";
+
+int openingPosition = message.IndexOf('(');
+int closingPosition = message.IndexOf(')');
+
+openingPosition += 1;
+
+int length = closingPosition - openingPosition;
+Console.WriteLine(message.Substring(openingPosition, length));
+*/
+
+
+/*
+string message = "What is the value <span>between the tags</span>?";
+
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
+
+int openingPosition = message.IndexOf(openSpan);
+int closingPosition = message.IndexOf(closeSpan);
+
+openingPosition += openSpan.Length;
+int length = closingPosition - openingPosition;
+Console.WriteLine(message.Substring(openingPosition, length));
+*/
+
+
+string message = "hello there!";
+
+int first_h = message.IndexOf('h');
+int last_h = message.LastIndexOf('h');
+
+Console.WriteLine($"For the message: '{message}', the first 'h' is at position {first_h} and the last 'h' is at position {last_h}.");
