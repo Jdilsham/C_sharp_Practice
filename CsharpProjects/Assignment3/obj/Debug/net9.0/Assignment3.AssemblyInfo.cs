82ad93b08f8a068fe0c2f58507c3ce9a002b114d
Jdilsham <janithadilsham@gmail.com>
2025-06-01 15:36:39 +0530

    inherit

--- CsharpProjects/Assignment3/obj/Debug/net9.0/Assignment3.AssemblyInfo.cs
+++ CsharpProjects/Assignment3/obj/Debug/net9.0/Assignment3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2fda80ac12dc870be67dff5ad12c0981a9c8df6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1aa0c277cdd14caa658f7dd90545cc721364ea6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
