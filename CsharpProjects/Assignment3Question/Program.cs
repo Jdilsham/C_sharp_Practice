7bbc75c8ab40fc7f02dac02ec9ec2a78b2775ec9
Jdilsham <janithadilsham@gmail.com>
2025-06-03 23:57:17 +0530

    saving

--- CsharpProjects/Assignment3Question/Program.cs
+++ CsharpProjects/Assignment3Question/Program.cs
@@ -1,1 +1,47 @@
-﻿
+﻿public class BankAccount
+{
+    public string AccountNumber { get; set; }
+    public double Balance { get; set; }
+
+    public BankAccount(string accountNumber, double balance)
+    {
+        AccountNumber = accountNumber;
+        Balance = balance;
+    }
+
+    public virtual void Deposite(double amount)
+    {
+        Balance += amount;
+        Console.WriteLine($"Deposited {amount:C}. New Balance: {Balance:C}");
+    }
+
+    public virtual void Withdraw(double amount)
+    {
+        if (amount <= Balance)
+        {
+            Balance -= amount;
+            Console.WriteLine($"Withdraw {amount:C}. New Balance: {Balance:C}");
+        }
+        else
+        {
+            Console.WriteLine("Insuffient funds.");
+        }
+    }
+}
+
+public class SavingsAccount : BankAccount
+{
+    public double InterestRate { get; set; }
+
+    public SavingsAccount(string accountNumber, double interestRate, double balance) : base(accountNumber, interestRate)
+    {
+        InterestRate = interestRate;
+    }
+
+    public void ApplyInterest()
+    {
+        double interest = InterestRate / 100;
+        Deposite(interest);
+        Console.WriteLine($"Interest applied at {InterestRate}%. New Balance: {Balance:C}");
+    }
+}
