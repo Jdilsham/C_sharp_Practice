6fc25a107ebae69464b96359b25921111617ff5b
Jdilsham <janithadilsham@gmail.com>
2025-06-09 21:49:05 +0530

    multiplier

---
+++ CsharpProjects/WindowsFormsApp1/Form3.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class timeTable : Form
+    {
+        public timeTable()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(textBox1.Text);
+            int num2 = int.Parse(textBox2.Text);
+            int mul = int.Parse(comboBox1.Text);
+
+            for (int i = num1; i <= num2; i++) { 
+            
+                int ans = i * mul;
+                string display = $"{i} x {mul} = {ans}";
+                listBox1.Items.Add(display);
+
+            }
+
+        }
+    }
+}
