af39f2b29a8d324948483bde50eb692b3650f9c4
Jdilsham <janithadilsham@gmail.com>
2025-06-18 21:55:13 +0530

    create

---
+++ CsharpProjects/Design_Patterns/SingletonDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = new Singleton();
+            s1.PrintDetails("This is First Message");
+        }
+    }
+}
