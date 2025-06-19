5ef502d69576fd31e173cd1a840d97c0df72dfe7
Jdilsham <janithadilsham@gmail.com>
2025-06-19 12:40:25 +0530

    celcius to fehrentheit

--- Design_Patterns/StaticDemo/Program.cs
+++ Design_Patterns/StaticDemo/Program.cs
@@ -10,6 +10,13 @@ namespace StaticDemo
     {
         static void Main(string[] args)
         {
+            double celcius = 37;
+            double fehrenheit = 98.6;
+
+            Console.WriteLine("Value of {0} celcius to fahrenheit is {1}",celcius,Converter.ToFahrenheit(celcius));
+            Console.WriteLine("Value of {0} fahrenheit to celcius is {1}",fehrenheit,Converter.ToCelcius(fehrenheit));
+
+            Console.ReadLine();
         }
     }
 }
