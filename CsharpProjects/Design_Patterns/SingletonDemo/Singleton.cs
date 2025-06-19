9a843a22a71d7d60a4500c7a60931d1a1cf5da1e
Jdilsham <janithadilsham@gmail.com>
2025-06-19 12:21:43 +0530

    eager

--- CsharpProjects/Design_Patterns/SingletonDemo/Singleton.cs
+++ CsharpProjects/Design_Patterns/SingletonDemo/Singleton.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace SingletonDemo
 {
+    //Lazy Singleton
+    /*
     public sealed class Singleton
     {
         private static int count = 0;
@@ -37,6 +39,59 @@ namespace SingletonDemo
         }
 
         public void PrintDetails(string message) { 
+            Console.WriteLine(message);
+        }
+    }
+    */
+
+    //Eager Singleton
+    /*
+    public sealed class Singleton
+    {
+        private static int count = 0;
+        private readonly static Singleton instance = new Singleton();
+        public static Singleton GetInstance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+        private Singleton()
+        {
+            count++;
+            Console.WriteLine("Counter value: " + count.ToString());
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+    */
+
+
+
+    //Eager singleton to Lazy Singleton using Lazy Keyword
+    public sealed class Singleton
+    {
+        private static int count = 0;
+        private readonly static Lazy<Singleton> instance = new Lazy<Singleton>(()=> new Singleton());
+        public static Singleton GetInstance
+        {
+            get
+            {
+                return instance.Value;
+            }
+        }
+        private Singleton()
+        {
+            count++;
+            Console.WriteLine("Counter value: " + count.ToString());
+        }
+
+        public void PrintDetails(string message)
+        {
             Console.WriteLine(message);
         }
     }
