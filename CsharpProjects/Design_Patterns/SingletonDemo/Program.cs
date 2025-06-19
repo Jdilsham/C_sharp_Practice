fc2e372096bd20c038971b47f071e8a9488598df
Jdilsham <janithadilsham@gmail.com>
2025-06-19 12:03:35 +0530

    paralal

--- CsharpProjects/Design_Patterns/SingletonDemo/Program.cs
+++ CsharpProjects/Design_Patterns/SingletonDemo/Program.cs
@@ -10,12 +10,36 @@ namespace SingletonDemo
     {
         static void Main(string[] args)
         {
-            Singleton fromEmployee = Singleton.GetInstance;
+            /*Singleton fromEmployee = Singleton.GetInstance;
             fromEmployee.PrintDetails("From Employee");
 
             Singleton fromStudent = Singleton.GetInstance;
             fromStudent.PrintDetails("From Student");
             Console.ReadLine();
+            */
+
+
+            //Getinstance in paralal
+            //This violate the singleton principles
+            Parallel.Invoke(
+                    () => PrintEmployeeDetails(),
+                    () => PrintStudentDetails()
+            );
+           
+
+            Console.ReadLine();
+        }
+
+        private static void PrintEmployeeDetails()
+        {
+            Singleton fromEmployee = Singleton.GetInstance;
+            fromEmployee.PrintDetails("From Employee");
+        }
+
+        private static void PrintStudentDetails()
+        {
+            Singleton fromStudent = Singleton.GetInstance;
+            fromStudent.PrintDetails("From Student");
         }
     }
 }
--- CsharpProjects/Design_Patterns/SingletonDemo/Singleton.cs
+++ CsharpProjects/Design_Patterns/SingletonDemo/Singleton.cs
@@ -9,15 +9,24 @@ namespace SingletonDemo
     public sealed class Singleton
     {
         private static int count = 0;
+        private static readonly object obj = new object();
         private static Singleton instance = null;
         public static Singleton GetInstance
         {
             get
-            { 
-                if (instance == null)
+            {
+                if (instance == null)//locke methode is very expencive, so we need it if instance is null only
                 {
-                    instance = new Singleton();
-                }    
+                    lock (obj) //only one thread can enter this code block at a given point of time, both employee and student are racing 
+                               //with each other in multi-threded environment lock ensure that only one thread which arrives earlier succeeds to enter
+                               //the code block and other will wait till the first one is completed
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Singleton();
+                        }
+                    }
+                }
                 return instance;
             }
         }
