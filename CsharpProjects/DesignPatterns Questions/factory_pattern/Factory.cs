586901cdc63df0c33b4b080b68523c7790b92e77
Jdilsham <>
2025-07-20 00:22:00 +0530

    add

---
+++ CsharpProjects/DesignPatterns Questions/factory_pattern/Factory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factory_pattern
+{
+    internal class Factory
+    {
+        public Shape GetShape(string shapeType) {
+
+            if (string.IsNullOrEmpty(shapeType))
+            {
+                return null;
+            }
+            if (shapeType.Equals("CIRCLE", StringComparison.OrdinalIgnoreCase){
+                return new Circle();
+            }
+            if (shapeType.Equals("SQUARE", StringComparison.OrdinalIgnoreCase))
+            {
+                return new Square();
+            }
+            if (shapeType.Equals("RECTANGLE", StringComparison.OrdinalIgnoreCase))
+            {
+                return new Rectangle();
+            }
+
+            return null;
+        }
+    }
+}
