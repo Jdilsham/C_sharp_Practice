9b7d2b53659a0cde2d3c80dad018a1eae5988e07
Jdilsham <154776437+Jdilsham@users.noreply.github.com>
2025-07-26 13:13:00 +0530

    Update Program.cs

--- CsharpProjects/DesignPatterns Questions/ProxyPattern/Program.cs
+++ CsharpProjects/DesignPatterns Questions/ProxyPattern/Program.cs
@@ -1,7 +1,17 @@
 ﻿using ProxyPattern;
 using System;
+using System.Linq.Expressions;
 
 public class Client
 {
+    static void Main(string[] args)
+    {
+        InternetAccess internet = new ProxyInternetAccess();
 
+        try
+        {
+            internet.Connect("Jani.com");
+            internet.Connect("jani.com");
+        }
+    }
 }
