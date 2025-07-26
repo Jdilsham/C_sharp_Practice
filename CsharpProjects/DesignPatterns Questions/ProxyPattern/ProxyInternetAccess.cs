3b2f5aa0af94cbf26f644ffd59700db2cae85033
Jdilsham <154776437+Jdilsham@users.noreply.github.com>
2025-07-26 12:54:05 +0530

    add

---
+++ CsharpProjects/DesignPatterns Questions/ProxyPattern/ProxyInternetAccess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern
+{
+    public class ProxyInternetAccess : InternertAccess
+    {
+        private RealInternetAccess realInternetAccess;
+        private List<string> restrictedSites;
+
+        public ProxyInternetAccess()
+        {
+            realInternetAccess = new RealInternetAccess();
+            restrictedSites = new List<string> { "jani.com", "dil.com" };
+        }
+
+
+    }
+}
--- CsharpProjects/DesignPatterns Questions/ProxyPattern/obj/Debug/net8.0/ProxyPattern.AssemblyInfo.cs
+++ CsharpProjects/DesignPatterns Questions/ProxyPattern/obj/Debug/net8.0/ProxyPattern.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProxyPattern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5be61930cb58bc33b334900d141c99517a182a48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bd972f160880e11517f2e79d594895c1dcf23b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProxyPattern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProxyPattern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
