49d2efe94d9683e24f0d2937d478a4ca4e3eb5bd
Jdilsham <154776437+Jdilsham@users.noreply.github.com>
2025-07-27 15:35:16 +0530

    add

---
+++ CsharpProjects/DesignPatterns Questions/Stratergy/TextEditor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stratergy
+{
+    public class TextEditor 
+    {
+        private ITextFormatter _textFormatter;
+
+        public void SetFormatter(ITextFormatter formatter)
+        {
+            _textFormatter = formatter;
+        }
+
+        public void PublishText(string text)
+        {
+            if (_textFormatter != null) 
+            {
+                string result = _textFormatter.TxtFormat(text);
+                Console.WriteLine("Formatted Text: " + result);
+            }
+            else
+            {
+                Console.WriteLine("No Formatter Set");
+            }
+
+    }
+}
--- CsharpProjects/DesignPatterns Questions/Stratergy/obj/Debug/net8.0/Stratergy.AssemblyInfo.cs
+++ CsharpProjects/DesignPatterns Questions/Stratergy/obj/Debug/net8.0/Stratergy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Stratergy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc3f3b15568cbd0e256d183ea98e377057809f87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9346f078c60ee13b2ecacf69ebf6415d5043e8c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Stratergy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Stratergy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
