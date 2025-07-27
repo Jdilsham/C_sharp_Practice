13c43312acacb370bc44b42f0b86667330ba7d67
Jdilsham <154776437+Jdilsham@users.noreply.github.com>
2025-07-27 16:03:22 +0530

    s

--- CsharpProjects/DesignPatterns Questions/Stratergy2/obj/Debug/net8.0/Stratergy2.AssemblyInfo.cs
+++ CsharpProjects/DesignPatterns Questions/Stratergy2/obj/Debug/net8.0/Stratergy2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Stratergy2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc167ca221f1cc84a47bd20464db7197438f8dc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f865d25d15fffa496a37399b86770f20edc7728f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Stratergy2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Stratergy2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
