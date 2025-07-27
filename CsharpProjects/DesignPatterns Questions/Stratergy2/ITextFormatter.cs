95d45a1a7fbe18faf4871b98686aec5e0d35e588
Jdilsham <154776437+Jdilsham@users.noreply.github.com>
2025-07-27 15:54:00 +0530

    add

---
+++ CsharpProjects/DesignPatterns Questions/Stratergy2/ITextFormatter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stratergy2
+{
+    internal interface ITextFormatter
+    {
+    }
+}
