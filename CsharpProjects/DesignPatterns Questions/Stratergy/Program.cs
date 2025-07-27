18c955372e50126ae77ef87265292396047cd08c
Jdilsham <154776437+Jdilsham@users.noreply.github.com>
2025-07-27 15:40:37 +0530

    Update Program.cs

--- CsharpProjects/DesignPatterns Questions/Stratergy/Program.cs
+++ CsharpProjects/DesignPatterns Questions/Stratergy/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using Stratergy;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TextEditor editor = new TextEditor();
+        string input = Console.ReadLine();
+
+        editor.SetFormatter(new UpperCaseFormatter());
+        editor.PublishText(input);
+
+        editor.SetFormatter(new LowerCaseFormatter());
+        editor.PublishText(input);
+    }
+}
