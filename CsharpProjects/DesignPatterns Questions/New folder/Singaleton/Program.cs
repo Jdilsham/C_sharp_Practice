205296bad48d6f58c5fd2fc6b652bf5c2d34da6f
Jdilsham <154776437+Jdilsham@users.noreply.github.com>
2025-07-28 12:58:30 +0530

    Update Program.cs

--- CsharpProjects/DesignPatterns Questions/New folder/Singaleton/Program.cs
+++ CsharpProjects/DesignPatterns Questions/New folder/Singaleton/Program.cs
@@ -8,15 +8,15 @@ public class program
         DatabaseConnection d1 = DatabaseConnection.Instance;
         DatabaseConnection d2 = DatabaseConnection.Instance;
 
-        if(ReferenceEquals(d1, d2))
+        if (ReferenceEquals(d1, d2))
         {
             Console.WriteLine("Same Connection");
         }
         else
         {
             Console.WriteLine("Differnt Connection");
-        
 
+        }
         d1.Connect();
         d2.Disconnect();
     }
