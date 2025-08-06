3df2c15ddc1b32ead3c886c976e91cee0a90c049
Jdilsham <154776437+Jdilsham@users.noreply.github.com>
2025-08-06 23:24:56 +0530

    Update Program.cs

--- CsharpProjects/DesignPatterns Questions/New folder/Singleton/Program.cs
+++ CsharpProjects/DesignPatterns Questions/New folder/Singleton/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Singleton;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        DatabaseConnection d1 = DatabaseConnection.Instance;
+        DatabaseConnection d2 = DatabaseConnection.Instance;
+
+        if(ReferenceEquals(d1, d2))
+        {
+            Console.WriteLine("Both Instance are the same instance!");
+        }
+        else
+        {
+            Console.WriteLine("Instance are different!");
+        }
+
+        d1.Connect();
+        d2.Disconnect();
+    }
+
+}
