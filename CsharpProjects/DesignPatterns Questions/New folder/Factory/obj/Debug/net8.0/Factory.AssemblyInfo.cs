ba1efa4403b9a51965d2a3b81a9434e32d0ee06f
Jdilsham <154776437+Jdilsham@users.noreply.github.com>
2025-08-09 00:16:03 +0530

    circle

--- CsharpProjects/DesignPatterns Questions/New folder/Factory/obj/Debug/net8.0/Factory.AssemblyInfo.cs
+++ CsharpProjects/DesignPatterns Questions/New folder/Factory/obj/Debug/net8.0/Factory.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Factory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89894397e28521ca049c123b5428e9a966cf1c9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d54b84d5e351110813256f3480928028f4eaab75")]
 [assembly: System.Reflection.AssemblyProductAttribute("Factory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Factory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
