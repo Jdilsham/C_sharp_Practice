cd6fd829fd48deffd60750b3e75c36d36a8924e9
Jdilsham <154776437+Jdilsham@users.noreply.github.com>
2025-08-09 00:43:37 +0530

    Update Program.cs

--- CsharpProjects/DesignPatterns Questions/New folder/Factory/Program.cs
+++ CsharpProjects/DesignPatterns Questions/New folder/Factory/Program.cs
@@ -5,6 +5,19 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        ShapeFactory factory = new ShapeFactory();
 
-    }
+        Console.WriteLine("Circle, Rectangle or Square ?");
+        string type = Console.ReadLine();
+
+        Shape shape = factory.getShape(type);
+
+        if (shape != null)
+        {
+            shape.Draw();
+        }
+        else
+        {
+            Console.WriteLine("Invalid");
+        }
 }
