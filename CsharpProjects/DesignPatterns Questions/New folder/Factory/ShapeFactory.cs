2496528109ca662af6f86f65e53d525c968d34cf
Jdilsham <154776437+Jdilsham@users.noreply.github.com>
2025-08-09 00:23:48 +0530

    factory

---
+++ CsharpProjects/DesignPatterns Questions/New folder/Factory/ShapeFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    internal class ShapeFactory
+    {
+        public Shape getShape(string shapeType)
+        {
+            if (string.IsNullOrEmpty(shapeType))
+            {
+                return null;
+            }
+
+            string shape = shapeType.ToUpper();
+
+            if (shape == "CIRCLE")
+            {
+                return new Circle();
+            }
+            
+            if (shape == "SQUARE")
+            {
+                return new Square();
+            }
+
+            if (shape == "RECTANGLE")
+            {
+                return new Rectangle();
+            }
+
+            return null;
+
+        }
+    }
+}
--- CsharpProjects/DesignPatterns Questions/New folder/Factory/obj/Debug/net8.0/Factory.AssemblyInfo.cs
+++ CsharpProjects/DesignPatterns Questions/New folder/Factory/obj/Debug/net8.0/Factory.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Factory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8073de3ceb0feec2e44ec89e9cdf36f5fa5cfe0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbb1075212130466b42e405a918869e7320538cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Factory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Factory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
