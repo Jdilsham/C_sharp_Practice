811eda533e79d05282f134e06433262489bf8f2c
Jdilsham <154776437+Jdilsham@users.noreply.github.com>
2025-08-11 23:11:17 +0530

    Update Program.cs

--- CsharpProjects/DesignPatterns Questions/New folder/ProxyPattern/Program.cs
+++ CsharpProjects/DesignPatterns Questions/New folder/ProxyPattern/Program.cs
@@ -1,3 +1,19 @@
 ﻿using System;
 using ProxyPattern;
 
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        InternetAccess internet = new ProxyInternetAccess();
+
+        try
+        {
+            internet.Connect("janitha.com");
+            internet.Connect("jani.com");
+        }catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
