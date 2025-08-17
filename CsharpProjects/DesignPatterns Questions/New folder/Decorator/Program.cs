d4f8dd46c3007e0af75f2df799433f6a254af92b
Jdilsham <154776437+Jdilsham@users.noreply.github.com>
2025-08-17 16:39:39 +0530

    Update Program.cs

--- CsharpProjects/DesignPatterns Questions/New folder/Decorator/Program.cs
+++ CsharpProjects/DesignPatterns Questions/New folder/Decorator/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+using Decorator;
+
+class program
+{
+    public static void Main(string[] args)
+    {
+        Shape Circle = new Circle();
+        Shape Rect = new Rectangle();
+
+        Circle.Draw()
+    }
+}
