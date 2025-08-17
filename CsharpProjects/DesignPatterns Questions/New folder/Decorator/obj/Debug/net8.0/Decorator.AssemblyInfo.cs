580e3ded86f4279847267dcca5121ac913cf8b78
Jdilsham <154776437+Jdilsham@users.noreply.github.com>
2025-08-17 16:21:15 +0530

    add

--- CsharpProjects/DesignPatterns Questions/New folder/Decorator/obj/Debug/net8.0/Decorator.AssemblyInfo.cs
+++ CsharpProjects/DesignPatterns Questions/New folder/Decorator/obj/Debug/net8.0/Decorator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Decorator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1caa619d1cc8fc523a0ae935ae485085e8a3a03f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd38ac6e34201ddd85a3d218ea5e847418ba8150")]
 [assembly: System.Reflection.AssemblyProductAttribute("Decorator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Decorator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
