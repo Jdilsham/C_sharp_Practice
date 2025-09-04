ebf8261503d1c30f3dbccd8696e99fba728c1d1b
Jdilsham <154776437+Jdilsham@users.noreply.github.com>
2025-09-04 23:08:39 +0530

    add

--- CsharpProjects/DesignPatterns Questions/New folder/Factory/Factory.cs
+++ CsharpProjects/DesignPatterns Questions/New folder/Factory/Factory.cs
@@ -8,9 +8,31 @@ namespace Factory
 {
     internal class Factory
     {
-        public Shape getShape()
+        public Shape getShape(string shape)
         {
+            if (string.IsNullOrEmpty(shape))
+            {
+                return null;
+            }
 
+            string shapeUpper = shape.ToUpper();
+
+            if(shapeUpper == "CIRCLE")
+            {
+                return new Circle();
+            }
+
+            if(shapeUpper == "Rect")
+            {
+                return new Rect();
+            }
+
+            if(shapeUpper == "SQUARE")
+            {
+                return new Shape();
+            }
+
+            return null;
         }
     }
 }
