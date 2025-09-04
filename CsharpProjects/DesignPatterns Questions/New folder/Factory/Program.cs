ae6e8449ee346aa9568886a32f4bb4db0dee3005
Jdilsham <154776437+Jdilsham@users.noreply.github.com>
2025-09-04 23:11:23 +0530

    Update Program.cs

--- CsharpProjects/DesignPatterns Questions/New folder/Factory/Program.cs
+++ CsharpProjects/DesignPatterns Questions/New folder/Factory/Program.cs
@@ -1,1 +1,15 @@
 ﻿using System;
+using Factory;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Factory factory = new Factory();
+
+        Console.WriteLine("What Shape do you want? ");
+        string shapetype = Console.ReadLine();
+
+
+    }
+}
