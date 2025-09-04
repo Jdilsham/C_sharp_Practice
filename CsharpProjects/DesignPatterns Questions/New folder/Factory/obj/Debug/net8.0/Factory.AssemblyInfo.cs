238e0faf088579ed6e018923fbfc4776d142e31b
Jdilsham <154776437+Jdilsham@users.noreply.github.com>
2025-09-04 23:03:29 +0530

    add

--- CsharpProjects/DesignPatterns Questions/New folder/Factory/obj/Debug/net8.0/Factory.AssemblyInfo.cs
+++ CsharpProjects/DesignPatterns Questions/New folder/Factory/obj/Debug/net8.0/Factory.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Factory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4262bf29ee962f007248924e2f46c7eaa86a2382")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc52a1aca525226f8adf5ce8bfc70bece5bc4e02")]
 [assembly: System.Reflection.AssemblyProductAttribute("Factory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Factory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
