dffdd5c4f29bea6c53365503946a66a8ef329f31
Jdilsham <154776437+Jdilsham@users.noreply.github.com>
2025-09-04 23:12:45 +0530

    Update Program.cs

--- CsharpProjects/DesignPatterns Questions/New folder/Factory/Program.cs
+++ CsharpProjects/DesignPatterns Questions/New folder/Factory/Program.cs
@@ -10,6 +10,16 @@ public class Program
         Console.WriteLine("What Shape do you want? ");
         string shapetype = Console.ReadLine();
 
+        Shape shape = factory.getShape(shapetype);
+
+        if (shape != null)
+        {
+            shape.Draw();
+        }
+        else
+        {
+            Console.WriteLine("Invalid!");
+        }
 
     }
 }
