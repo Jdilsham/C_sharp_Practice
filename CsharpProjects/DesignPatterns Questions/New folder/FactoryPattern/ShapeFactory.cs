2556e33b0c571d6b9f817814dd14dde0f911999f
Jdilsham <154776437+Jdilsham@users.noreply.github.com>
2025-09-05 22:00:56 +0530

    add

---
+++ CsharpProjects/DesignPatterns Questions/New folder/FactoryPattern/ShapeFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    internal class ShapeFactory
+    {
+        public Shape getShape(string shapeType)
+        {
+            if (string.IsNullOrEmpty(shapeType))
+            {
+                return null;
+            }
+
+            string shapeUpper = shapeType.ToUpper();
+
+            if(shapeUpper == "Circle")
+            {
+                return new Circle();
+            }
+
+            if(shapeUpper == "RECTANFLE")
+            {
+                return new Rectangle();
+            }
+
+            if(shapeUpper == "SQUARE")
+            {
+                return new Square();
+            }
+
+            return null;
+        }
+    }
+}
