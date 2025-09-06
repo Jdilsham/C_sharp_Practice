a13013e94d505a87ea908605dfd73355f64958af
Jdilsham <154776437+Jdilsham@users.noreply.github.com>
2025-09-06 21:57:21 +0530

    add

---
+++ CsharpProjects/DesignPatterns Questions/New folder/FactoryVehicle/IVehicle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryVehicle
+{
+    public interface IVehicle
+    {
+        int NumberOfWheel();
+        string VehicleType();
+    }
+}
