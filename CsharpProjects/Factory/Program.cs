648b853cc50e20f1340863ebba599e2ad47c5eb6
Jdilsham <154776437+Jdilsham@users.noreply.github.com>
2025-09-14 20:55:04 +0530

    add

---
+++ CsharpProjects/Factory/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Factory;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        ShapeFactory factory = new ShapeFactory();
+
+        Console.WriteLine("Circle or Square ?");
+        string input = Console.ReadLine();
+
+        Shape shape = factory.getShape(input);
+
+        if (shape != null)
+        {
+            shape.Draw();
+        }
+        else
+        {
+            Console.WriteLine("Invalid");
+        }
+    }
+}
---
+++ CsharpProjects/Factory/ShapeFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    internal class ShapeFactory
+    {
+        public Shape getShape(string shape)
+        {
+
+            if (string.IsNullOrEmpty(shape))
+            {
+                return null;
+            }
+
+            string shapeUpper = shape.ToUpper();
+
+            if (shapeUpper == "CIRCLE")
+            {
+                return new Circle();
+            }
+
+            if (shapeUpper == "SQUARE")
+            {
+                return new Square();
+            }
+
+            return null;
+        }
+    }
+}
