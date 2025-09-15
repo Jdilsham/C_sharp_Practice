9c7a1933623d553b1f2b69837aea0cc33cd7304b
Jdilsham <154776437+Jdilsham@users.noreply.github.com>
2025-09-15 15:44:54 +0530

    add

---
+++ CsharpProjects/DecoratorPractice/CustomerStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPractice
+{
+    internal class CustomerStore
+    {
+        List<Customer> customers = new List<Customer>();
+    }
+}
